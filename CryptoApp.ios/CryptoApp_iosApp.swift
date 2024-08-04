48bb661cc289df0541af66f76dff6ddadbba0069
Hossain Muktar <HossainMuktarDev@gmail.com>
2024-08-05 01:49:49 +0600

    initial commit

---
+++ CryptoApp.ios/CryptoApp_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp_iosApp.swift
+//  CryptoApp.ios
+//
+//  Created by Hossain Muktar on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
